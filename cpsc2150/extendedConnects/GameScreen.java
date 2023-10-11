3c7a843d64adbe6f0249d36379153c8a7b4e11c4
Cgfrazi <cgfrazi@clemson.edu>
2023-10-11 18:13:50 -0400

    Updated GameScreen

--- cpsc2150/extendedConnects/GameScreen.java
+++ cpsc2150/extendedConnects/GameScreen.java
@@ -7,14 +7,63 @@ Kalyaan Narnamalpuram Srinivasan (Jingleez)
 Rowan Froeschner (Rojofroe)
  */
 
+import cpsc2150.extendedConnectX.models.GameBoard;
+
+import java.util.Scanner;
+
 /**
  *
  */
-public class GameScreen {
+public class GameScreen
+{
 
     public static void main(String[] args)
     {
-
+        Scanner keyboard = new Scanner(System.in);
+        GameBoard newBoard = new GameBoard();
+        int userChoice = 0;
+        while (!newBoard.checkForWin(userChoice))
+        {
+            System.out.println(newBoard.toString());
+            System.out.println("Player X, what column do you want to place your marker in?");
+            userChoice = keyboard.nextInt();
+            while (userChoice < 0 || userChoice > newBoard.getNumColumns() || !newBoard.checkIfFree(userChoice))
+            {
+                if (userChoice < 0)
+                {
+                    System.out.println("Column cannot be less than 0");
+                }
+                else if (userChoice > newBoard.getNumColumns())
+                {
+                    System.out.println("Column cannot be greater than " + newBoard.getNumColumns());
+                }
+                else if (!newBoard.checkIfFree(userChoice))
+                {
+                    System.out.println("Column is full" + newBoard.getNumColumns());
+                }
+                System.out.println("Player X, what column do you want to place your marker in?");
+                userChoice = keyboard.nextInt();
+            }
+            System.out.println(newBoard.toString());
+            System.out.println("Player O, what column do you want to place your marker in?");
+            userChoice = keyboard.nextInt();
+            while (userChoice < 0 || userChoice > newBoard.getNumColumns() || !newBoard.checkIfFree(userChoice))
+            {
+                if (userChoice < 0)
+                {
+                    System.out.println("Column cannot be less than 0");
+                }
+                else if (userChoice > newBoard.getNumColumns())
+                {
+                    System.out.println("Column cannot be greater than " + newBoard.getNumColumns());
+                }
+                else if (!newBoard.checkIfFree(userChoice))
+                {
+                    System.out.println("Column is full" + newBoard.getNumColumns());
+                }
+                System.out.println("Player O, what column do you want to place your marker in?");
+                userChoice = keyboard.nextInt();
+            }
+        }
     }
-
 }
